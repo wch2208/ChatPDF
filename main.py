a9678703f0a945dc78b77efd7c27f27018085d53
wch2208 <97877328+wch2208@users.noreply.github.com>
2023-08-26 21:19:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings

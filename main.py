c18180e466f9cf3e520cadae67ae05f87afc0cd7
wch2208 <97877328+wch2208@users.noreply.github.com>
2023-08-26 21:38:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,6 @@ import os
 st.title("ChatPDF")
 st.write("---")
 
-#OpenAI KEY 입력 받기
-# openai_key = st.text_input('OPENAI_API_KEY', type="password")
-
-
 # 파일 업로드
 uploaded_file = st.file_uploader("PDF 파일을 올려주세요", type=["pdf"])
 st.write("---")
